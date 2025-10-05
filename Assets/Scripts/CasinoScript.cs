a4e4d48aa2bc9ae15c7806eda3351eeb63e47add
jonathanhj123 <jona246@gmail.com>
2025-10-05 13:26:10 +0200

    Added casinoscene with full functionality

--- Assets/Scripts/CasinoScript.cs
+++ Assets/Scripts/CasinoScript.cs
@@ -1,3 +1,5 @@
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,7 +8,17 @@ public class CasinoScript : MonoBehaviour
     private Label _Card;
     private VisualElement _casinoUIRoot;
     private Button[] _buttons = new Button[4];
-    public Sprite[] _cards = new Sprite[4]; // 0 = Es, 1 = Konge, 2 = Dame, 3 = Bonde.
+    [SerializeField] private Sprite[] Cards = new Sprite[4]; // 0 = Es, 1 = Konge, 2 = Dame, 3 = Bonde.
+    private int _Score = 0;
+    private Label _ScoreLabel;
+    private int _HighScore = 0;
+    private Label _HighScoreLabel;
+    private bool _HasAnswered = false;
+    private int _Random = 0;
+    private float _wait = 0;
+    [SerializeField] public int _timer = 1;
+    [SerializeField] private Sprite _CardBack;
+
     void Start()
     {
 
@@ -16,35 +28,117 @@ public class CasinoScript : MonoBehaviour
         _buttons[1] = _casinoUIRoot.Q<Button>("Answer2");
         _buttons[2] = _casinoUIRoot.Q<Button>("Answer3");
         _buttons[3] = _casinoUIRoot.Q<Button>("Answer4");
+        _HighScoreLabel = _casinoUIRoot.Q<Label>("HighScore");
+        _ScoreLabel = _casinoUIRoot.Q<Label>("Score");
+        _ScoreLabel.text = "Score: 0";
+        _HighScoreLabel.text = "HighScore: 0";
 
 
-        UpdateCasinoCard();
-        SetRightAnswer();
+
+
+        _buttons[0].text = "Es";
+        _buttons[1].text = "Konge";
+        _buttons[2].text = "Dame";
+        _buttons[3].text = "Bonde";
+
+
+        SetAnswerAndCard();
     }
 
 
     void Update()
     {
 
+        if (_HasAnswered)
+        {
+
+            if (_wait < _timer)
+            {
+                _wait = _wait + Time.deltaTime;
+            }
+            else
+            {
+                ResetColors();
+                SetAnswerAndCard();
+                _HasAnswered = false;
+                _wait = 0;
+            }
+        }
     }
 
-    public void UpdateCasinoCard()
-    {
-        // update the card to show what the card is.
-    }
 
-    public void SetRightAnswer()
+    public void SetAnswerAndCard()
     {
-        // set the right button with the right answer
+        _Random = Random.Range(0, 4);
+        _Card.style.backgroundImage = Background.FromSprite(_CardBack);
+
+        for (int i = 0; i < _buttons.Length; i++)
+        {
+            _buttons[i].clicked -= RightAnswer;
+            _buttons[i].clicked -= WrongAnswer;
+            if (_Random == i)
+            {
+                _buttons[i].clicked += RightAnswer;
+            }
+            else
+            {
+                _buttons[i].clicked += WrongAnswer;
+            }
+        }
+        ResetColors();
+        Debug.Log(_Random);
     }
 
     public void RightAnswer()
     {
-        // Show it was right answer, give them a point
+        _Score++;
+        UpdateScore();
+        Debug.Log("Right");
+        setColors();
+        _HasAnswered = true;
+        _Card.style.backgroundImage = Background.FromSprite(Cards[_Random]);
     }
 
     public void WrongAnswer()
     {
-        // Shot it was wrong answer end the streak and go back to the level select
+        Debug.Log("Wrong");
+        setColors();
+        if (_Score > _HighScore)
+        {
+            _HighScore = _Score;
+            _HighScoreLabel.text = "High Score: " + _HighScore.ToString();
+        }
+        _HasAnswered = true;
+        _Card.style.backgroundImage = Background.FromSprite(Cards[_Random]);
+        _Score = 0;
+        UpdateScore();
+    }
+
+
+    public void setColors()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (_Random == i)
+            {
+                _buttons[i].style.color = Color.green;
+            }
+            else
+            {
+                _buttons[i].style.color = Color.red;
+            }
+        }
+    }
+
+    public void ResetColors()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            _buttons[i].style.color = Color.black;
+        }
+    }
+    public void UpdateScore()
+    {
+        _ScoreLabel.text = "Score : " + _Score.ToString();   
     }
 }
